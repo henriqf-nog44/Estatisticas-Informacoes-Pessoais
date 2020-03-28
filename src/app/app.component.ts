814528da0d2d2ce7d5e2daa21b39f42d84569445
Henrique <henrique.nogueira@rede.ulbra.br>
2020-03-28 17:05:37 -0300

    1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EIP';
+  nome = null;
+  idade = null;
+  pessoas = [];
+  pessoaMaisJovem = null;
+  pessoaMaisVelha = null;
+
+
+  salvar() {
+    this.pessoas.push({
+      nome: this.nome,
+      idade: this.idade
+    });
+    this.nome = null;
+    this.idade = null;
+    this.atualizarEstatísticas();
+  }
+
+  remover(pessoa) {
+    const i = this.pessoas.indexOf(pessoa);
+    this.pessoas.splice(i, 1);
+    this.atualizarEstatísticas();
+  }
+
+  atualizarEstatísticas(){
+    let copia = Object.create(this.pessoas);
+    copia.sort((p1:any, p2:any)=> p1.idade - p2.idade);
+    this.pessoaMaisJovem = copia[0];
+
+    copia.sort((p1:any, p2:any)=> p2.idade - p1.idade);
+    this.pessoaMaisVelha = copia[0];
+  }
 }
